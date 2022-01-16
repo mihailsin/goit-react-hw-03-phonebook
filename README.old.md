b05c3a58ecdcd267b8078e816928854af23452d2
mihailsin <mihailsin88@gmail.com>
2022-01-16 12:06:37 +0200

    initial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# goit-react-hw-02-phonebook
+3rd task of 3rd homework; react course
---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import ContactForm from './Components/ContactForm';
+import ContactList from './Components/ContactList';
+import Filter from './Components/Filter';
+
+import './App.css';
+import { Grid, GridContainer } from './App.styled';
+
+class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  filterInputHandler = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getContactNames = () => {
+    return this.state.contacts.map(contact => contact.name.toLowerCase());
+  };
+
+  handleSubmittedData = contact => {
+    const existingNames = this.getContactNames();
+
+    if (existingNames.includes(contact.name.toLowerCase())) {
+      alert(`${contact.name} is already in contacts`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [contact, ...prevState.contacts],
+      }));
+    }
+  };
+
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedContacts = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedContacts),
+    );
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const filteredContacts = this.filterContacts();
+    return (
+      <Grid>
+        <GridContainer>
+          <h1>Phonebook</h1>
+          <ContactForm submitted={this.handleSubmittedData} />
+        </GridContainer>
+
+        <GridContainer>
+          <h2>Contacts</h2>
+          <Filter
+            value={this.state.filter}
+            inputHandler={this.filterInputHandler}
+          />
+          <ContactList
+            contacts={filteredContacts}
+            deleteHandler={this.deleteContact}
+          />
+        </GridContainer>
+      </Grid>
+    );
+  }
+}
+
+export default App;
